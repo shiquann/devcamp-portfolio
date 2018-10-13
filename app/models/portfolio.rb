5fb315d694ae9c9110ee995faa37691b5af20dee
shiquann <shiquann>
2018-10-13 11:39:43 -0500

    Implemented calidations for blogs, skills, and portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,2 +1,3 @@
 class Portfolio < ApplicationRecord
+    validates_presence_of :title, :main_image, :body, :thumb_image
 end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,2 +1,3 @@
 class Skill < ApplicationRecord
+    validates_presence_of :percent_utilized, :title
 end
