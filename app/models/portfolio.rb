d65ff716dfc2c85e18d4168474b6a89edbe96240
shiquann <shiquann>
2018-10-13 21:59:01 -0500

    implemented custom scopes for portfolio items

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,9 @@
 class Portfolio < ApplicationRecord
     validates_presence_of :title, :main_image, :body, :thumb_image
+
+    def self.angular
+        where(subtitle: "Angular")
+    end
+
+    scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
 end
